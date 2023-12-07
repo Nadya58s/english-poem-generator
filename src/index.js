70186d1885321b9aa235f234e08dd70d9a66dfb6
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-12-07 15:49:47 +0000

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 /** @format */
 
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: " BleBlaBlaBla Poem",
+    autoStart: true,
+    delay: 1,
+    cursor: ",",
+  });
+
+  poemElement.innerHTML = " bla bla bla poem";
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
